c1c5cd1bd7e50062486c934d5d2eac2a890618f2
charles Gift <charlesgiftangila@gmail.com>
2024-07-06 11:46:22 +0300

    adding events to todolist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("form").addEventListener("submit", (ev) => {
+    ev.preventDefault();
+    toDoList(ev.target.new_task.value);
+  });
 });
+function toDoList(todoitems) {
+  let p = document.createElement("p");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", toDelete);
+  btn.textContent = "delete";
+  p.textContent = `${todoitems}`;
+  p.appendChild(btn);
+  console.log(p);
+  document.querySelector("#list").appendChild(p);
+}
+function toDelete(ev) {
+  ev.target.parentNode.remove();
+}
